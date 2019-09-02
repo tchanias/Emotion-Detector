adf1ab6cad6d9df06a842a5e70a72b3b0200e850
tchanias <tchanias@gmail.com>
2019-09-03 00:55:41 +0300

    https://westus.dev.cognitive.microsoft.com/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236

--- Emotion-Detector/constants/constants.js
+++ Emotion-Detector/constants/constants.js
@@ -1,3 +1,27 @@
-export const subKey = ''
+export const subKey = '2bfab510b0f54217b9b3f06cf810af12';
+export const faceApi = 'https://emotion-detector.cognitiveservices.azure.com/face/v1.0/detect';
+// export const faceApi = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect?';
+export const parameters ={
+    returnFaceId: "true",
+    returnFaceLandmarks: "false",
+    returnFaceAttributes:
+        "age,gender,headPose,smile,facialHair,glasses,emotion," +
+        "hair,makeup,occlusion,accessories,blur,exposure,noise"
+}
+export const headers = {
+    "Content-Type":"application/json",
+    "Ocp-Apim-Subscription-Key": subKey
+    // "Subscription-Key": subKey
+}
 
+export function parseUrlParams(urlParams) {
+    const joinByEquals = (pair) => pair.join('=')
+    const params = Object.entries(urlParams).map(joinByEquals).join('&')
+    if (params) {
+        return `?${params}`
+    } else {
+    return ''
+    }
+}
 
+export const apiUrl = faceApi +'?'+'returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age%2Cgender%2CheadPose%2Csmile%2CfacialHair%2Cglasses%2Cemotion%2Chair%2Cmakeup%2Cocclusion%2Caccessories%2Cblur%2Cexposure%2Cnoise'
--- Emotion-Detector/screens/Recognition.js
+++ Emotion-Detector/screens/Recognition.js
@@ -3,8 +3,12 @@ import { Text,Image ,Button ,  View } from 'react-native';
 import { Permissions} from 'expo';
 import  Constants from 'expo-constants';
 import * as ImagePicker from 'expo-image-picker';
-// import {subKey} from ''
+import {apiUrl,headers} from '../constants/constants';
+import axios from 'axios';
 
+
+ 
+ const apiheaders={headers:headers};
 export class Recognition extends Component {
     state = {
         image: null,
@@ -12,68 +16,64 @@ export class Recognition extends Component {
 
       componentDidMount() {
         this.getPermissionAsync();
-        console.log(ImagePicker)
       }
 
+      
 
       processImageForRecognition=()=>{
-        var subscriptionKey = "<Subscription Key>";
-    
-        // NOTE: You must use the same region in your REST call as you used to
-        // obtain your subscription keys. For example, if you obtained your
-        // subscription keys from westus, replace "westcentralus" in the URL
-        // below with "westus".
-        //
-        // Free trial subscription keys are generated in the "westus" region.
-        // If you use a free trial subscription key, you shouldn't need to change 
-        // this region.
-        var uriBase =
-            "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
-    
-        // Request parameters.
-        var params = {
-            "returnFaceId": "true",
-            "returnFaceLandmarks": "false",
-            "returnFaceAttributes":
-                "age,gender,headPose,smile,facialHair,glasses,emotion," +
-                "hair,makeup,occlusion,accessories,blur,exposure,noise"
-        };
+      
+       
     
         // Display the image.
-        var sourceImageUrl = document.getElementById("inputImage").value;
-        document.querySelector("#sourceImage").src = sourceImageUrl;
-    
+        // var sourceImageUrl = document.getElementById("inputImage").value;
+        // document.querySelector("#sourceImage").src = sourceImageUrl;
+        let sourceImageUrl = this.state.image;
+        axios.post(apiUrl,{
+          data: '{"url": ' + '"' + sourceImageUrl + '"}  '
+        },
+        apiheaders)
+        .then((response)=>{
+          console.log(response,'api call response')
+        })
+        .catch((error)=>{
+          console.log('imageUrl: ',sourceImageUrl)
+          console.log('apiUrl: ',apiUrl)
+          console.log('data: ','{"url": ' + '"' + sourceImageUrl + '"}  ')
+          console.log('headers: ',apiheaders);
+          console.log('An error occured: ',error)
+        })
         // Perform the REST API call.
-        $.ajax({
-            url: uriBase + "?" + $.param(params),
+        // $.ajax({
+        //     url: uriBase + "?" + $.param(params),
     
-            // Request headers.
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            },
+        //     // Request headers.
+        //     beforeSend: function(xhrObj){
+        //         xhrObj.setRequestHeader("Content-Type","application/json");
+        //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        //     },
     
-            type: "POST",
+        //     type: "POST",
     
-            // Request body.
-            data: '{"url": ' + '"' + sourceImageUrl + '"}',
-        })
+        //     // Request body.
+        //     data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        // })
     
-        .done(function(data) {
-            // Show formatted JSON on webpage.
-            $("#responseTextArea").val(JSON.stringify(data, null, 2));
-        })
+        // .done(function(data) {
+        //     // Show formatted JSON on webpage.
+        //     // $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        //     console.log(data)
+        // })
     
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            // Display error message.
-            var errorString = (errorThrown === "") ?
-                "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-            errorString += (jqXHR.responseText === "") ?
-                "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
-                    jQuery.parseJSON(jqXHR.responseText).message :
-                        jQuery.parseJSON(jqXHR.responseText).error.message;
-            alert(errorString);
-        });
+        // .fail(function(jqXHR, textStatus, errorThrown) {
+        //     // Display error message.
+        //     var errorString = (errorThrown === "") ?
+        //         "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        //     errorString += (jqXHR.responseText === "") ?
+        //         "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+        //             jQuery.parseJSON(jqXHR.responseText).message :
+        //                 jQuery.parseJSON(jqXHR.responseText).error.message;
+        //     alert(errorString);
+        // });
       }
 
       getPermissionAsync = async () => {
